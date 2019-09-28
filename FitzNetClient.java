995fe067ff1342416d24f0fe20b0db6173359d98
BuildTools <unconfigured@null.spigotmc.org>
2019-09-28 07:08:41 +0000

    Server and Client now talk! Added comments.

--- FitzNetClient.java
+++ FitzNetClient.java
@@ -4,49 +4,44 @@ import java.util.*;
 
 public class FitzNetClient {
     static Socket socket;
-    public static void main(String[] args)throws IOException{
+    private static ObjectInputStream obIn = null;
+
+    public static void main(String[] args) throws IOException,ClassNotFoundException,OptionalDataException{
         // Var declarations
-        //This takes the localIP of the machine
+        // This takes the localIP of the machine
         InetAddress host = InetAddress.getLocalHost();
         Scanner sysIn = new Scanner(System.in);
         Socket socket = null;
-        ObjectInputStream obIn = null;
         ObjectOutputStream obOut = null;
 
         System.out.println("Fitz-Net Client Alpha 1.0");
 
-        while(true){
-        //Create Socket
-        socket = new Socket(host.getHostName(),5000);
-        //Use this to write to socket
-        System.out.println("Starting OutputStream");
-        obOut = new ObjectOutputStream(socket.getOutputStream());
-        System.out.println("Attached");
-        //Use this to recive messages from the Server
-        System.out.println("Starting InStream.");
-        obIn = new ObjectInputStream(socket.getInputStream());
-        System.out.println("Attached");
-        String message = "";
-        System.out.println("Enter message to send to server");
-        message = sysIn.nextLine();
-        
-        obOut.writeObject(message);
-        
-        //Cleaning House
-        obOut.close();
-        obIn.close();
-        socket.close();
+        while (true) {
+            try {
+                // Create Socket
+                socket = new Socket(host.getHostName(), 5000);
+                // Connect Streams to server
+                obOut = new ObjectOutputStream(socket.getOutputStream());
+                obIn = new ObjectInputStream(socket.getInputStream());
+                System.out.println((String)obIn.readObject());
+                //Send Message/Data to server
+                String message = "";
+                System.out.println("Enter message to send to server");
+                message = sysIn.nextLine();
+                //Write to outstream
+                obOut.writeObject(message);
+                obOut.flush();
 
+                // Cleaning House
+                sysIn.close();
+                obOut.close();
+                obIn.close();
+                socket.close();
+            //Catch exceptions
+            } catch (ConnectException e) {
+                System.out.println(e.getMessage());
         }
     }
 
-/*
-Timer t = new Timer();
-t.schedule(new TimerTask() {
-    @Override
-    public void run() {
-       System.out.println("Hello World");
-    }
-}, 0, 5000);
-*/
+}
 }
--- FitzNetServer.java
+++ FitzNetServer.java
@@ -1,33 +1,45 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
-public class FitzNetServer{
+public class FitzNetServer {
     private static ServerSocket server;
     private static int PORT = 5000;
-    public static void main(String[] args)throws IOException{
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         System.out.println("FitzNet Server v1.0");
-        //Create ServerSocket obj
+        // Create ServerSocket obj
         server = new ServerSocket(PORT);
-        //Always listening...
+        // Always listening...
 
-        while(true){
-            System.out.println("Waiting for connection.");
-            //Create socket and wait for client to connect
-            Socket socket = server.accept();
-            System.out.println(socket.getInetAddress() + " Connected !");
-            //Take the sockets input stream (from client)
-            System.out.println("Starting InStream.");
-            DataInputStream obIn = new DataInputStream(socket.getInputStream());
-            //Sending info back to client
-            System.out.println("Starting OutputStream");
-            DataOutputStream obOut = new DataOutputStream(socket.getOutputStream());
-            //Convert ObjectInputStream to strings
-            String message = (String) obIn.readUTF();
-            System.out.println("Client: " + message);
+        while (true) {
+            try {
+                System.out.println("Waiting for connection.");
+                // Create socket and wait for client to connect
+                Socket socket = server.accept();
+                System.out.println(socket.getInetAddress() + " Connected !");
+                ObjectOutputStream obOut = new ObjectOutputStream(socket.getOutputStream());
+                // Take the sockets input stream (from client)
+                ObjectInputStream obIn = new ObjectInputStream(socket.getInputStream());
+                // Send a message to client saying its connected
+                obOut.writeObject("Connected.");
+                obOut.flush();
+                // Print out String of object
+                System.out.println((String) obIn.readObject());
 
-            //Cleaning House
-            obIn.close();
-            socket.close();
+                String message = "";
+                // Read the message from the client and print
+                message += (String) obIn.readObject();
+                System.out.println("Client: " + message);
+
+                // Cleaning House
+                obIn.close();
+                obOut.close();
+                socket.close();
+            } catch (EOFException exception) {
+                System.out.println("Client disconnect.");
+                System.out.println();
+            }
         }
     }
 }
