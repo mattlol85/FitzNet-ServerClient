3a60ea41ec1a705adff92c3695010f7c5aef6017
BuildTools <unconfigured@null.spigotmc.org>
2019-09-28 02:45:08 +0000

    Added start scripts and almost fixed the data transmission

--- FitzNetClient.java
+++ FitzNetClient.java
@@ -1,13 +1,52 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class FitzNetClient {
     static Socket socket;
     public static void main(String[] args)throws IOException{
+        // Var declarations
+        //This takes the localIP of the machine
+        InetAddress host = InetAddress.getLocalHost();
+        Scanner sysIn = new Scanner(System.in);
+        Socket socket = null;
+        ObjectInputStream obIn = null;
+        ObjectOutputStream obOut = null;
+
         System.out.println("Fitz-Net Client Alpha 1.0");
-        socket = new Socket("localhost",5000);
-        DataInputStream dIn = new DataInputStream(socket.getInputStream());
+
+        while(true){
+        //Create Socket
+        socket = new Socket(host.getHostName(),5000);
+        //Use this to write to socket
+        System.out.println("Starting OutputStream");
+        obOut = new ObjectOutputStream(socket.getOutputStream());
+        System.out.println("Attached");
+        //Use this to recive messages from the Server
+        System.out.println("Starting InStream.");
+        obIn = new ObjectInputStream(socket.getInputStream());
+        System.out.println("Attached");
+        String message = "";
+        System.out.println("Enter message to send to server");
+        message = sysIn.nextLine();
+        
+        obOut.writeObject(message);
+        
+        //Cleaning House
+        obOut.close();
+        obIn.close();
         socket.close();
+
+        }
     }
 
+/*
+Timer t = new Timer();
+t.schedule(new TimerTask() {
+    @Override
+    public void run() {
+       System.out.println("Hello World");
+    }
+}, 0, 5000);
+*/
 }
--- FitzNetServer.java
+++ FitzNetServer.java
@@ -2,17 +2,32 @@ import java.net.*;
 import java.io.*;
 
 public class FitzNetServer{
-    final static int PORT = 5000;
+    private static ServerSocket server;
+    private static int PORT = 5000;
     public static void main(String[] args)throws IOException{
         System.out.println("FitzNet Server v1.0");
-        createSocket();
+        //Create ServerSocket obj
+        server = new ServerSocket(PORT);
+        //Always listening...
 
-    }
-    public static void createSocket() throws IOException{
-        ServerSocket serverSocket = new ServerSocket(PORT);
-        Socket clientSocket = serverSocket.accept();
-        System.out.println("Client Connected.");
-        System.out.println(clientSocket.getInetAddress());
-        serverSocket.close();
+        while(true){
+            System.out.println("Waiting for connection.");
+            //Create socket and wait for client to connect
+            Socket socket = server.accept();
+            System.out.println(socket.getInetAddress() + " Connected !");
+            //Take the sockets input stream (from client)
+            System.out.println("Starting InStream.");
+            DataInputStream obIn = new DataInputStream(socket.getInputStream());
+            //Sending info back to client
+            System.out.println("Starting OutputStream");
+            DataOutputStream obOut = new DataOutputStream(socket.getOutputStream());
+            //Convert ObjectInputStream to strings
+            String message = (String) obIn.readUTF();
+            System.out.println("Client: " + message);
+
+            //Cleaning House
+            obIn.close();
+            socket.close();
+        }
     }
 }
